04ca1c17ae5a5a595f336bd089c60b093982d662
qwercik <erykandrzejewski@gmail.com>
2019-02-08 17:17:45 +0100

    Instalacja Symfony w ścieżce projektu
    Zainstalowany Symfony w wersji 4.2 w ścieżce projektu. Skonfigurowano serwer Apache, aby DocumentRoot znajdował się w katalogu public. Jednocześnie z instalacją usunięto przykładowy plik public/index.php testujący połączenie z bazą danych.

--- public/index.php
+++ public/index.php
@@ -1,8 +1,27 @@
 <?php
 
-$dsn = 'mysql:host=rekrutacja-mysql;dbname=rekrutacja';
-$user = 'rekrutacja';
-$password = 'rekrutacja';
+use App\Kernel;
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
-$pdo = new PDO($dsn, $user, $password);
-echo 'Wersja MySQL: ' . $pdo->getAttribute(PDO::ATTR_SERVER_VERSION);
+require dirname(__DIR__).'/config/bootstrap.php';
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
+}
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
