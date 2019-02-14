170a8ce3e67a367d79b708dd7ec2d52d764409f5
qwercik <erykandrzejewski@gmail.com>
2019-02-14 21:40:15 +0100

    Usunięcie zbędnej tabeli z migracji
    Usunięto przypadkowo utworzoną tabelę z kodu SQL migracji (tabela Test).

--- src/Migrations/Version20190210155646.php
+++ src/Migrations/Version20190210155646.php
@@ -23,7 +23,6 @@ final class Version20190210155646 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE post (id INT AUTO_INCREMENT NOT NULL, ordinal INT NOT NULL, topic VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, creation_date DATETIME NOT NULL, active TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci ENGINE = InnoDB');
-        $this->addSql('DROP TABLE Test');
     }
 
     public function down(Schema $schema) : void
@@ -31,7 +30,6 @@ final class Version20190210155646 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE Test (id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = MyISAM COMMENT = \'\' ');
         $this->addSql('DROP TABLE post');
     }
 }
